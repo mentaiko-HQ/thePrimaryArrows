32b53722de23ff443220dc387cc5c2ec5d8d6993
mentaiko-HQ <y.hayata0201@gmail.com>
2025-12-12 17:47:17 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+// ここにURLとKEYを一度だけ書けば、全てのファイルで使い回せます
+
+const APP_CONFIG = {
+  URL: 'https://aetenreumweohzjqqcpf.supabase.co', // ← ここに実際のURLを貼る
+  KEY: 'sb_publishable__0UiwcqGL8b6OwX828Lh0A_eteYgUoi', // ← ここに実際のKEYを貼る
+};
